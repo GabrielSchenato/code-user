77cec967b21fc9988ff232911cc4b259a220e937
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-13 16:34:13 -0300

    Criando usuário com senha criptografada

---
+++ tests/CodeUser/Repository/UserRepositoryTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace CodePress\CodeUser\Tests;
+
+use CodePress\CodeUser\Repository\UserRepositoryEloquent;
+use CodePress\CodeUser\Tests\AbstractTestCase;
+use Illuminate\Support\Facades\Hash;
+use Mockery as m;
+
+/**
+ * Description of UserRepositoryTest
+ *
+ * @author gabriel
+ */
+class UserRepositoryTest extends AbstractTestCase
+{
+    
+    private $repository;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->migrate();
+        $this->repository = new UserRepositoryEloquent();
+    }
+    
+    public function test_can_create_user()
+    {
+        $user = $this->repository->create([
+           'name' => 'Teste',
+            'email' => 'teste@teste.com',
+            'password' => '123456'
+        ]);
+        
+        $this->assertEquals('Teste', $user->name);
+        $this->assertEquals('teste@teste.com', $user->email);
+        $this->assertTrue(Hash::check('123456', $user->password));
+    }
+}
