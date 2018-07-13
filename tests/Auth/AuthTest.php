5aec632c1b68f4f7fe59d61710707c82625a6441
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-13 09:47:38 -0300

    Testando a autenticação

--- tests/Auth/AuthTest.php
+++ tests/Auth/AuthTest.php
@@ -4,6 +4,8 @@ namespace CodePress\CodeUser\Tests\Auth;
 
 use CodePress\CodeUser\Tests\AbstractTestCase;
 use Illuminate\Support\Facades\Auth;
+use Orchestra\Testbench\Concerns\WithFactories;
+use CodePress\CodeUser\Models\User;
 
 /**
  * Description of AuthTest
@@ -13,15 +15,25 @@ use Illuminate\Support\Facades\Auth;
 class AuthTest extends AbstractTestCase
 {
 
+    use WithFactories;
+
     protected function setUp()
     {
         parent::setUp();
         $this->migrate();
+        $this->withFactories(__DIR__ . '/../../src/resources/factories');
     }
 
-    public function test_check_if_not_authenticated()
+    public function test_check_if_is_not_authenticated()
     {
         $this->assertEquals(false, Auth::check());
+    }
+
+    public function test_check_if_is_authenticated()
+    {
+        $user = factory(User::class)->create();
+        Auth::login($user);
+        $this->assertEquals(true, Auth::check());
     }
 
 }
