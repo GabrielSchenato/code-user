6d1af24ddefcbefb73ac64ce99d110ff0d239f04
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-16 10:42:29 -0300

    Criando template para email de registro

--- src/CodeUser/Providers/CodeUserServiceProvider.php
+++ src/CodeUser/Providers/CodeUserServiceProvider.php
@@ -20,6 +20,7 @@ class CodeUserServiceProvider extends ServiceProvider
         $this->publishes([__DIR__ . '/../../config/auth.php' => base_path('config/auth.php')], 'config');
         $this->publishes([__DIR__ . '/../../resources/migrations/' => base_path('database/migrations')], 'migrations');
         $this->publishes([__DIR__ . '/../../resources/views/auth' => base_path('resources/views/auth')], 'auth');
+        $this->publishes([__DIR__ . '/../../resources/views/email' => base_path('resources/views/email')], 'email');
         
         $this->loadViewsFrom(__DIR__ . '/../../resources/views/codeuser', 'codeuser');
         require __DIR__ . '/../routes.php';
---
+++ src/resources/views/email/registration.blade.php
@@ -0,0 +1,14 @@
+<h3>CodePress</h3>
+
+<p>
+    Sua conta na plataforma foi criada com sucesso.
+</p>
+<p>
+    Usuário: <strong>{{ $username }}</strong>
+</p>
+<p>
+    Senha: <strong>{{ $password }}</strong>
+</p>
+Acesse o sistema e altere sua senha.
+<br>
+Obs.: Não responda este e-mail, ele é gerado automaticamente
