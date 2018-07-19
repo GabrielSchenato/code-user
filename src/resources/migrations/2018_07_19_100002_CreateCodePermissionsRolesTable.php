bc2c7cbe7a2f9781839266892c969abcb59cc5c0
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-19 09:24:17 -0300

    Criando migrations ACL

---
+++ src/resources/migrations/2018_07_19_100002_CreateCodePermissionsRolesTable.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCodePermissionsRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('codepress_permission_roles', function (Blueprint $table) {
+            $table->integer('permission_id');
+            $table->foreign('permission_id')->references('id')->on('codepress_permissions');
+            $table->integer('role_id');
+            $table->foreign('role_id')->references('id')->on('codepress_roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('codepress_permission_roles');
+    }
+}
