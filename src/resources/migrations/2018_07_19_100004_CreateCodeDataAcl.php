cc1e0d767624190b2c37564a484f74e85d280306
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-19 16:13:05 -0300

    Definindo permissões de acesso na migration

--- src/resources/migrations/2018_07_19_100004_CreateCodeDataAcl.php
+++ src/resources/migrations/2018_07_19_100004_CreateCodeDataAcl.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use CodePress\CodeUser\Models\Role;
 use CodePress\CodeUser\Models\Permission;
+
 class CreateCodeDataAcl extends Migration
 {
     /**
@@ -29,7 +30,30 @@ class CreateCodeDataAcl extends Migration
             'description' => 'Permissão para publicação de posts que estão em rascunho'
         ]);
         
+        $permissionAccessCategories = Permission::create([
+            'name' => 'access_categories',
+            'description' => 'Permissão para o acesso a área de categorias'
+        ]);
+        
+        $permissionAccessTags = Permission::create([
+            'name' => 'access_tags',
+            'description' => 'Permissão para o acesso a área de tags'
+        ]);
+        
+        $permissionAccessUsers = Permission::create([
+            'name' => 'access_users',
+            'description' => 'Permissão para o acesso a área de usuários'
+        ]);
+        
+        $permissionAccessPosts = Permission::create([
+            'name' => 'access_posts',
+            'description' => 'Permissão para o acesso a área de posts'
+        ]);
+        
         $roleEditor->permissions()->save($permissionPublishPost);
+        $roleEditor->permissions()->save($permissionAccessPosts);
+        
+        $roleRedator->permissions()->save($permissionAccessPosts);
     }
 
     /**
