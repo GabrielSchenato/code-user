45edc9b1ad63861c9a6d3f6f1714ba540e1950de
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2018-07-23 17:25:57 -0300

    correção dos tests

--- src/resources/migrations/2018_07_19_100005_CreateUsers.php
+++ src/resources/migrations/2018_07_19_100005_CreateUsers.php
@@ -6,6 +6,7 @@ use CodePress\CodeUser\Models\User;
 
 class CreateUsers extends Migration
 {
+
     /**
      * Run the migrations.
      *
@@ -14,14 +15,34 @@ class CreateUsers extends Migration
     public function up()
     {
         $roleAdmin = Role::where('name', Role::ROLE_ADMIN)->first();
-        
+
         $admin = User::create([
-            'name' => 'Administrador',
-            'email' => 'admin@codepress.com',
-            'password' => bcrypt(123456)
+                    'name' => 'Administrador',
+                    'email' => 'admin@codepress.com',
+                    'password' => bcrypt(123456)
         ]);
-        
+
         $admin->roles()->save($roleAdmin);
+
+        $roleEditor = Role::where('name', Role::ROLE_EDITOR)->first();
+
+        $editor = User::create([
+                    'name' => 'Editor',
+                    'email' => 'editor@codepress.com',
+                    'password' => bcrypt(123456)
+        ]);
+
+        $editor->roles()->save($roleEditor);
+
+        $roleRedator = Role::where('name', Role::ROLE_REDATOR)->first();
+
+        $redator = User::create([
+                    'name' => 'Redator',
+                    'email' => 'redator@codepress.com',
+                    'password' => bcrypt(123456)
+        ]);
+
+        $redator->roles()->save($roleRedator);
     }
 
     /**
@@ -31,5 +52,7 @@ class CreateUsers extends Migration
      */
     public function down()
     {
+        
     }
+
 }
